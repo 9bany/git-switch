bfe4ddc9b2e5e44ef249c7bf100a1b0f9ac384bd
9bany <bany.y0599@gmail.com>
2022-05-02 11:06:13 +0700

    swgit : comment ft

---
+++ bin/main.js
@@ -0,0 +1,2 @@
+// clone
+// commit : a
---
+++ switch_control/index.js
@@ -0,0 +1,14 @@
+// SaveNewUser(user *dbstore.User)
+
+// UpdateUser(user *dbstore.User)
+// DeleteUser(user *dbstore.User)
+// ListUser(user *dbstore.User)
+
+// UpdateUserDefault(user *dbstore.User)
+// GetUserDefault(user *dbstore.User)
+
+// SwitchUser(user *dbstore.User)
+
+// CheckUserRule(user *dbstore.User)
+
+// Forward() -> git

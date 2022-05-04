e28ab6d40e6f7d6f1e87778a71f9248eced28af3
9bany <bany.y0599@gmail.com>
2022-05-04 15:02:22 +0700

    swgit: add example

--- bin/options.js
+++ bin/options.js
@@ -6,10 +6,11 @@ function options() {
     .command('curl <url>', 'fetch the contents of the URL', () => {}, (argv) => {
         console.info(argv)
     })
+    // node bin/index.js --add.user=<username> --add.email=<email>
     .option("a", {
         alias: "add", 
         describe: "New account", 
-        type: "string", 
+        type: "object", 
         demandOption: false 
     })
     .option("g", {

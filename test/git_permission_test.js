129bd8ed0359308e29fbad52f25e1df73289c625
9bany <bany.y0599@gmail.com>
2022-05-07 17:52:55 +0700

    change url to http

--- test/git_permission_test.js
+++ test/git_permission_test.js
@@ -3,13 +3,13 @@ const assert = require('assert');
 
 describe('git:git permission', function () { 
     it(`should return true when have permission with public repo`, function () {
-        return checkGitPermission("git@github.com:9bany/git-switch.git").then(data => {
+        return checkGitPermission("https://github.com/9bany/git-switch.git").then(data => {
             assert.equal(data, true)
         })
     })
 
     it(`should return false when access private repo`, function () {
-        return checkGitPermission("git@github.com:olli-ai/ios_app.git").then(data => {
+        return checkGitPermission("https://github.com/olli-ai/ios_app.git").then(data => {
             assert.equal(data, false)
         })
     })

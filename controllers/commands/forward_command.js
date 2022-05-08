18f13052387c3ebdb6906cc0b1d479d4b0d9abe9
9bany <bany.y0599@gmail.com>
2022-05-08 20:45:36 +0700

    ft/implement forward command

---
+++ controllers/commands/forward_command.js
@@ -0,0 +1,42 @@
+const checkGitPermission = require('./../exc/git_permission');
+const runCommandWithGit = require('./../exc/run_command');
+const gitRemoteV = require('./../exc/git_remote_url');
+
+const { 
+    getURLString
+} = require('./../../utils/index');
+
+const {
+    COMMAND_ERR,
+    DONT_HAVE_PERMISSION
+} = require('./../../constants/global')
+
+async function forWardCommand(argv) {
+    
+    let url = await gitRemoteV().then(data => {
+        return getURLString(data)
+    }).catch(err => {
+        console.log(err)
+        return null
+    })
+
+    if (argv._.length === 0) {
+        console.error(COMMAND_ERR)
+    } else {
+        let command = argv._.join(" ");
+        checkGitPermission(url).then(isAllow => {
+            if(isAllow) {
+                runCommandWithGit(command)
+            } else {
+                console.error(DONT_HAVE_PERMISSION)
+            }
+        })
+    }
+
+    
+}
+
+
+
+
+module.exports = forWardCommand;
--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,10 @@
 const genUsername = require("unique-username-generator");
 var randomEmail = require('random-email');
 var randomUrl = require('random-url');
+const { 
+    EMPTY_STRING,
+    PARSING_ERR
+} = require('./../constants/global');
 
 module.exports = {
     randomUsername: () => {
@@ -11,5 +15,22 @@ module.exports = {
     },
     randomUrl: () => {
         return randomUrl('https');
+    },
+    getURLString: (str) => {
+        if (!str) {
+            return EMPTY_STRING
+        }
+        let stringToArrayWithWhiteSpace = str.split(' ');
+    
+        if (stringToArrayWithWhiteSpace.length == 0 || !stringToArrayWithWhiteSpace[0]) {
+            return PARSING_ERR
+        }
+        
+        let stringToArrayWithTab = stringToArrayWithWhiteSpace[0].split('\t')
+
+        if (stringToArrayWithTab.length == 0 || !stringToArrayWithTab[1]){ 
+            return PARSING_ERR
+        }
+        return stringToArrayWithTab[1]
     }
 }

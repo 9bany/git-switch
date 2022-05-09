7a65ac3d1f89f011a1640f0093e5f3c1c7bf3f26
9bany <bany.y0599@gmail.com>
2022-05-09 11:00:08 +0700

    test/remote-url-add failed case

--- test/git_remote_url_test.js
+++ test/git_remote_url_test.js
@@ -1,10 +1,26 @@
 const gitRemoteV  = require('../controllers/exc/git_remote_url');
+const { exec } = require("child_process");
 const assert = require('assert');
 
-describe('git:git remote -v', function () { 
-    it(`should return string when have git remote`, function () {
-        return gitRemoteV().then(data => {
-            assert.notEqual(data, null)
+main()
+
+function main() {
+    describe('git:git remote -v', function () { 
+        it(`should return string when have git remote`, function () {
+            return gitRemoteV().then(data => {
+                assert.notEqual(data, null)
+            })
+        })
+
+        it(`should return error when dont have git file`, function () {
+            return exec(`rm -rf .git`, (error, stdout, stderr) => {
+                return gitRemoteV().then(data => {
+                    assert.notEqual(data, null)
+                }).catch(err => {
+                    assert.notEqual(err, null)
+                })
+            })
+            
         })
     })
-})
+}

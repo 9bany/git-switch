075b4c89ad46b1e2a80629fbea14c9815ae0d244
9bany <bany.y0599@gmail.com>
2022-05-09 11:27:11 +0700

    fix run test only machine

--- test/git_remote_url_test.js
+++ test/git_remote_url_test.js
@@ -1,5 +1,6 @@
 const gitRemoteV  = require('../controllers/exc/git_remote_url');
 const { exec } = require("child_process");
+const { allowRunTestOnMachine } = require('./util')
 const assert = require('assert');
 
 main()
@@ -13,14 +14,18 @@ function main() {
         })
 
         it(`should return error when dont have git file`, function () {
-            return exec(`rm -rf .git`, (error, stdout, stderr) => {
-                return gitRemoteV().then(data => {
-                    assert.notEqual(data, null)
-                }).catch(err => {
-                    assert.notEqual(null, null)
-                })
-            })
-            
+            return runTestDeleteGitConfig()
         })
     })
+}
+
+async function runTestDeleteGitConfig() {
+    if (allowRunTestOnMachine()) {
+        await exec(`rm -rf .git`, (error, stdout, stderr) => { console.log("ls ok")})
+        return gitRemoteV().then(data => {
+            assert.notEqual(data, null)
+        }).catch(err => {
+            assert.notEqual(err, null)
+        })
+    }
 }

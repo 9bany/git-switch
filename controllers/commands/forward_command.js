701309e30c53acb35b0a935fec282a07dab6c181
9bany <bany.y0599@gmail.com>
2022-05-12 22:18:58 +0700

    92-optimize-forward-command-dont-check-permission-before-run-command

--- controllers/commands/forward_command.js
+++ controllers/commands/forward_command.js
@@ -1,21 +1,21 @@
-const checkGitPermission = require('./../exc/git_permission');
-const runCommandWithGit = require('./../exc/run_command');
-const gitRemoteV = require('./../exc/git_remote_url');
-
-const { 
-    getURLString
-} = require('./../../utils/index');
-
 const {
     COMMAND_ERR,
     DONT_HAVE_PERMISSION,
     REPO_DOES_NOT_EXISTS,
     USER_DOES_NOT_EXISTS
 } = require('./../../constants/global');
+
+const { 
+    getURLString
+} = require('./../../utils/index');
+
 const log = require('../../utils/log');
-const getRepo = require('../options/get_repo')
-const getUserInfoById = require('../options/get_user_info_by_id')
-const switchUser = require('../options/switch_user')
+const getRepo = require('../options/get_repo');
+const getUserInfoById = require('../options/get_user_info_by_id');
+const switchUser = require('../options/switch_user');
+const runCommandWithGit = require('./../exc/run_command');
+const gitRemoteV = require('./../exc/git_remote_url');
+
 async function forWardCommand(argv) {
     let commandKeys = Object.keys(argv)
     if (argv._.length === 0 && !(commandKeys.some(i => i !== '_' && i !== '$0'))) {
@@ -28,31 +28,11 @@ async function forWardCommand(argv) {
             return null
         })
         await autoSwitchUser(url)
-        checkGitPermission(url).then(isAllow => {
-            if(isAllow) {
-                const args = process.argv.slice(2);
-                let command = args.join(' ')
-                runCommandWithGit(command)
-            } else {
-                log.error(DONT_HAVE_PERMISSION)
-            }
-        })
+        const args = process.argv.slice(2);
+        let command = args.join(' ')
+        runCommandWithGit(command)
     }
 }
-
-
-const convertCommand = (obj) => {
-    let listKey = Object.keys(obj).filter(i => i!=='_' && i!=='$0')  
-    let command = obj._.join(" ")
-    listKey.forEach( i=> {
-        command += ' -'+i
-        if(typeof obj[i] === 'string'){
-            command += ` "${obj[i]}"`
-        }
-    })
-    return command
-}
-
 
 const autoSwitchUser = async (url) => {
     if(!Boolean(url)) return false

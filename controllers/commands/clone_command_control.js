5954ea01209e9abf5cfa200424c2ffe4ab39e99b
@bany <53461381+9bany@users.noreply.github.com>
2022-05-14 11:07:48 +0700

    91-ft-cli-loading (#128)
    * 91-ft-cli-loading
    
    * remove git-switch folder

--- controllers/commands/clone_command_control.js
+++ controllers/commands/clone_command_control.js
@@ -7,17 +7,23 @@ const getUserDefault = require('./../options/get_user_default')
 const switchUser = require('./../options/switch_user')
 const checkUserRule = require('./../options/check_user_rule')
 const createNewUser = require('./../options/create_new')
+
 const {
     COMMAND_ERR,
     DONT_HAVE_PERMISSION,
     USER_DOES_NOT_EXISTS,
     REPO_DOES_NOT_EXISTS,
-} = require('./../../constants/global')
+} = require('./../../constants/global');
+const log = require('../../utils/log');
+const { stopAnimation } = require('../../utils/loader');
 
 function cloneCommandControl(repo_url) {
     if (!Boolean(repo_url)) {
         console.error(COMMAND_ERR)
     } else {
+        
+        let loader = log.user.loading("Cloning...");
+
         checkGitPermission(repo_url).then(isAllow => {
             if(isAllow) {
                 runCommandWithGit(`clone ${repo_url}`).then(async () => {
@@ -36,6 +42,8 @@ function cloneCommandControl(repo_url) {
                         default:
                             userInfo = getUserDefault()
                     }
+                    
+                    stopAnimation(loader)
 
                     if(repoInfo == REPO_DOES_NOT_EXISTS){
                         createRepo({url:repo_url, userID: userInfo.id})
--- release.md
+++ release.md
@@ -2,3 +2,4 @@
 ###  Features
 - Switch user admin of the repository: [issue](https://github.com/9bany/git-switch/issues/99)
 - Create ssh key on window: [issue](https://github.com/9bany/git-switch/issues/39)
+- Loading then clone an repository: [issue](https://github.com/9bany/git-switch/issues/91)

9bb148053cb9d724b90843d2adb740d9c2cb897f
9bany <bany.y0599@gmail.com>
2022-05-16 13:51:57 +0700

    138-bump-handle-file-global-then-building-app

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const dotenv = require( "dotenv");
+const dotenv = require("dotenv");
 const path = require('path');
 const options = require('./options');
 const adapter = require('./adapter');
-const { 
+const {
     createNewUser,
     updateUser,
     deleteUser,
@@ -19,68 +19,87 @@ const {
 } = require('../controllers');
 const updateUserAdminRepo = require("../controllers/options/update_user_repo");
 const addExistRepo = require("../controllers/options/add_exist_repo");
+const {
+    initSystem,
+    checkInitSystem
+} = require("./init");
+const log = require("../utils/log");
 
-dotenv.config({path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 main();
 
 function main() {
     options()
-    .command('config', 'swgit config', () => {}, (argv)=> {
-        adapter(argv, [
-            {
-                type: 'update',
-                handle: updateUser
-            },
-            {
-                type: 'delete',
-                handle: deleteUser
-            },
-            {
-                type: 'list',
-                handle: listUser
-            },
-            {
-                type: 'default',
-                handle: getUserDefault
-            },
-            {
-                type: 'checkrule',
-                handle: checkUserRule
-            },
-        ])
-    })
-    .command('*', 'All command with swgit', () => {}, (argv) => {
-        adapter(argv, [
-            {
-                type: 'add',
-                handle: createNewUser
-            },
-            {
-                type: 'switch',
-                handle: switchUser
-            },
-            {
-                type: 'clone',
-                handle: cloneCommandControl
-            },
-            {
-                type: 'get',
-                handle: getUserInfo
-            },
-            {
-                type: 'user-repo',
-                handle: userRepo
-            },
-            {
-                type: 'update-ur',
-                handle: updateUserAdminRepo
-            },
-            {
-                type: 'add-repo',
-                handle: addExistRepo
-            },
-        ], forWardCommand)
-    })
-    .argv;
+        .command('init', 'Init your tool before use', () => {}, (argv) => {
+            initSystem().then(data => {
+                log.user.info("OKKKKKKKKK, `swgit --help`")
+            })
+        })
+        .command('config', 'swgit config', () => {}, (argv) => {
+            if (!checkInitSystem()) {
+                log.user.error("Please, run `swgit init` before use")
+                return
+            }
+            adapter(argv, [
+                {
+                    type: 'update',
+                    handle: updateUser
+                },
+                {
+                    type: 'delete',
+                    handle: deleteUser
+                },
+                {
+                    type: 'list',
+                    handle: listUser
+                },
+                {
+                    type: 'default',
+                    handle: getUserDefault
+                },
+                {
+                    type: 'checkrule',
+                    handle: checkUserRule
+                },
+            ])
+        })
+        .command('*', 'All command with swgit', () => {}, (argv) => {
+            if (!checkInitSystem()) {
+                log.user.error("Please, run `swgit init` before use")
+                return
+            } 
+            adapter(argv, [
+                {
+                    type: 'add',
+                    handle: createNewUser
+                },
+                {
+                    type: 'switch',
+                    handle: switchUser
+                },
+                {
+                    type: 'clone',
+                    handle: cloneCommandControl
+                },
+                {
+                    type: 'get',
+                    handle: getUserInfo
+                },
+                {
+                    type: 'user-repo',
+                    handle: userRepo
+                },
+                {
+                    type: 'update-ur',
+                    handle: updateUserAdminRepo
+                },
+                {
+                    type: 'add-repo',
+                    handle: addExistRepo
+                },
+            ], forWardCommand)
+        })
+        .argv;
+
 }

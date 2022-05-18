23a1c7186f6adcbdc849f9d4ad0883126de6dd37
@bany <53461381+9bany@users.noreply.github.com>
2022-05-18 17:43:23 +0700

    ft/cicd-test-on-win (#168)
    * ft/cicd-test-on-win
    
    * ft/cicd test on win (#171)
    
    * fix/cicd test on win (#1)
    
    * clone/cicd-test-on-win
    
    clone original workflow for window
    
    * install-cross-env-for-multi-os-env
    
    * fix/cicd test on win (#2)
    
    * clone/cicd-test-on-win
    
    clone original workflow for window
    
    * install-cross-env-for-multi-os-env
    
    * update-workflows-changes
    
    * update-path-resolve-os-directory
    
    * update-path-resolve-ssh-key-manage
    
    * ft/cicd test on win (#172)
    
    * update-copy-commands-window
    
    * remove-old-workflow-windows
    
    * re-enable-trigger-using-workflow-for-window
    
    Co-authored-by: BiBi <45527552+Bi-Bi-ic@users.noreply.github.com>

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,7 +1,8 @@
 const homedir = require('os').homedir();
+const path = require('path');
 module.exports = {
-    SSH_ROOT_PATH: homedir + '/.ssh',
-    SSH_CONFIG_PATH: homedir + '/.ssh/config',
+    SSH_ROOT_PATH: path.resolve(homedir + '/.ssh'),
+    SSH_CONFIG_PATH: path.resolve(homedir + '/.ssh/config'),
     HOST_DEFAULT: 'github.com',
     DB_CONFIG_PATH: 'swgit_store/db',
     DB_CONFIG_FILENAME: 'swgit.json',
